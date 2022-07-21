32f8e98c80f2047ce8144b2cd5b78cf2c6d5b44a
lee454ho <lee454ho@gmail.com>
2022-07-21 10:36:02 +0900

    leesangho

--- script.js
+++ script.js
@@ -55,56 +55,41 @@ footerScroll.addEventListener('click',function(e){
     }
 })
 ///////////////////////////////////////
+// 베스트셀러 책 캐러셀
+let bookitem = document.querySelectorAll(".bookItem");
+let bookslider = document.querySelector(".bestSeller-book-move")
 
-//슬라이드이벤트 셀렉터
-let btnR = document.querySelector("#btnR");
-let btnL = document.querySelector("#btnL");
-let item = document.querySelectorAll(".slideItem")
-let slider = document.querySelector(".bestSeller-sell-move")
+let bookfirstChild = bookslider.firstElementChild.cloneNode(true);
+let booklastChild = bookslider.lastChild.cloneNode(true);
+bookslider.append(bookfirstChild)
 
-let itemlength =item.length
-let itemIndex = 0
+let width = 0;
+let i = 0
 
-let firstChild = slider.firstElementChild;
-let firstclone = firstChild.cloneNode(true);
-
-console.log(slider)
-
-
-btnL.addEventListener("click",function(){
-    if(itemIndex <= item.length){
-        slider.style.transform = "translate(" + 100 * (itemIndex + 1)+"px)"
+setInterval(function(){    
+    if(bookitem.length >= i){
+        bookslider.style.transform = "translate3d(-"+163*(i+1)+"px, 0px, 0px)"
+        bookslider.style.transition = "0.2s"
+       
+        i++
     }
-    console.log(itemIndex)
-    item[++itemIndex]
-    
-    if(itemIndex === item.length -1){
-    setTimeout(function(){
+    if(i === 3){
+        setTimeout(function(){
+
+            bookslider.style.transition = "0s";
+            bookslider.style.transform = "translate3d(0px,0px,0px)";
+        },200)
+        i = 0
+    }
         
-            slider.style.transition = "0ms"
-            slider.style.transform = "translate(" + 100 * (itemIndex + 1)+"px)"
-        },300);
-        itemIndex = -1;
-    }
-    item[++itemIndex]
-        
-    
-    
-})
-btnR.addEventListener("click",function(){
-    if(itemIndex <= item.length - 1){
-        slider.style.transform = "translate(-" + 100 * (itemIndex + 1)+"px)"
-    }
-    item[++itemIndex]
-})
+},5000)
 
 
-//네비바 픽스트 이벤트
+//네비바 픽스드 이벤트
 let scrollNav_bar = document.querySelector(".nav-bar"); 
 
 window.addEventListener("scroll",function(){
-    console.log(window.pageYOffset)
-    if(window.pageYOffset > 30){
+    if(window.pageYOffset > 80){
         scrollNav_bar.className = "nav-bar-fixed"
     }
     else if(window.pageYOffset < 30){
@@ -116,23 +101,22 @@ window.addEventListener("scroll",function(){
 
 
 //네비바 스크롤 이동 이벤트 
-let scrollele = document.querySelectorAll(".scroll-ele");
-let scrollpotint = document.querySelectorAll(".scroll");
-let first = scrollpotint[0].offsetTop; 
-let second = scrollpotint[1].offsetTop;
-let third = scrollpotint[2].offsetTop;
-console.log(scrollele[1])
+// let scrollele = document.querySelectorAll(".scroll-ele");
+// let scrollpotint = document.querySelectorAll(".pointer");
+// let first = scrollpotint[0].offsetTop
+// let second = scrollpotint[1].offsetTop
+// let third = scrollpotint[2].offsetTop
+// console.log(first)
 
-scrollele[0].addEventListener("click",function(){
-    window.scroll({top:first,behavior:"smooth"});
-})
-scrollele[1].addEventListener("click",function(){
-    window.scroll({top:second,behavior:"smooth"});
-})
-scrollele[2].addEventListener("click",function(){
-    window.scroll({top:third,behavior:"smooth"});
-})
-
+// scrollele[0].addEventListener("click",function(){
+//     window.scrollTo({top:first,behavior:"smooth"});
+// })
+// scrollele[1].addEventListener("click",function(){
+//     window.scrollTo({top:second,behavior:"smooth"});
+// })
+// scrollele[2].addEventListener("click",function(){
+//     window.scrollTo({top:third,behavior:"smooth"});
+// })
 
 
 //////////////////상호//////////////////
